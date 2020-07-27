b9d0497d60717a7f82d16bc9d8f8390407d20d9f
Jonathan Yaniv Ben Avraham <jbenav200@caledonian.ac.uk>
2020-07-27 02:10:44 +0100

    Create Intray #2

---
+++ App/todoapp/lib/models/global.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+Color redColor = new Color(0xFFDC4F64);
+Color goldColor = new Color(0xFFFFD460);
+Color darkGray = new Color(0xFF212020);
+
+TextStyle intrayTitleStyle = new TextStyle(
+  fontFamily: 'Avenir',
+  fontWeight: FontWeight.bold,
+  fontSize: 40,
+  color: darkGray,
+);
